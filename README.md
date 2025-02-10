e2e0721df9410f22e40c18204933de01f766ea06
Valentine <64153441+valentine-carter@users.noreply.github.com>
2025-02-10 14:59:48 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# valentine-carter-xyz
+ 
---
+++ work/flight-path/flight_path.js
@@ -0,0 +1,108 @@
+
+var position = {x: 0, y: window.innerHeight/2};
+var counter = 0;
+var minFontSize = 9;
+var angleDistortion = 0;
+var letters = "these birds freighted with ancient purpose mark our skies as they pass overhead but their windswept fricatives are mere whispers as baneful smoke convulses from the stricken ground that has been blackened by those who born wingless baulk at flight ";
+
+var canvas;
+var context;
+var mouse = {x: 0, y: 0, down: false}
+
+function init() {
+  canvas = document.getElementById( 'canvas' );
+  context = canvas.getContext( '2d' );
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  canvas.addEventListener('mousemove', mouseMove, false);
+  canvas.addEventListener('mousedown', mouseDown, false);
+  canvas.addEventListener('mouseup',   mouseUp,   false);
+  canvas.addEventListener('mouseout',  mouseUp,  false);  
+  canvas.addEventListener('dblclick', doubleClick, false);
+  
+  window.onresize = function(event) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+}
+
+function mouseMove (){
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+  draw();
+}
+
+function draw() {
+ if ( mouse.down ) {
+    var d = distance( position, mouse );
+    var fontSize = minFontSize + d/2;
+    var letter = letters[counter];
+    var stepSize = textWidth( letter, fontSize );
+    
+    if (d > stepSize) {
+      var angle = Math.atan2(mouse.y-position.y, mouse.x-position.x);
+      
+      context.font = fontSize + "px Georgia";
+    
+      context.save();
+      context.translate( position.x, position.y);
+      context.rotate( angle );
+      context.fillText(letter,0,0);
+      context.restore();
+
+      counter++;
+      if (counter > letters.length-1) {
+        counter = 0;
+      }
+    
+
+      position.x = position.x + Math.cos(angle) * stepSize;
+      position.y = position.y + Math.sin(angle) * stepSize;
+
+      }
+  }     
+}
+
+function distance( pt, pt2 ){
+  
+  var xs = 0;
+  var ys = 0;
+ 
+  xs = pt2.x - pt.x;
+  xs = xs * xs;
+ 
+  ys = pt2.y - pt.y;
+  ys = ys * ys;
+ 
+  return Math.sqrt( xs + ys );
+}
+
+function mouseDown(){
+  mouse.down = true;
+  position.x = event.pageX;
+  position.y = event.pageY;
+  
+  document.getElementById('info').style.display = 'none';
+}
+
+function mouseUp(){
+    mouse.down = false;
+}
+
+function doubleClick() {
+  canvas.width = canvas.width; 
+}
+
+function textWidth( string, size ) {
+  context.font = size + "px Georgia";
+  
+  if ( context.fillText ) {
+    return context.measureText( string ).width;
+  } else if ( context.mozDrawText) {
+    return context.mozMeasureText( string );
+  }
+  
+ };
+
+init();
